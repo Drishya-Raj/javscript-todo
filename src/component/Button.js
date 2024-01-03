188aaea66d84f29d91b005cb674b0f85a82736ef
Drishya-Raj <145426042+Drishya-Raj@users.noreply.github.com>
2024-01-03 15:08:49 +0530

    Delete src directory

--- src/component/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Button({ onClick,value,className }) {
-    return (
-        <button className={className} onClick={onClick}>{value}</button>
-    );
-}
-
-export default Button;
